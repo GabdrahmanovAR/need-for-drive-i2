027f53bceabcb7e53a85f07ac85763892de73b38
Grosno <pbc20092yandex.ru>
2022-04-29 11:32:14 +0300

    F-10 Реализован компонент контейнер для одинаковых страниц со списком сущностей

--- src/components/admin-content/category/Category.tsx
+++ src/components/admin-content/category/Category.tsx
@@ -1,14 +1,11 @@
-import { Pagination } from 'antd';
-import React, { useEffect, useState } from 'react';
+import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import cn from 'classnames';
 import moment from 'moment';
-import Spinner from '../../Spinner/Spinner';
-import OrderFilters from '../order-filters/OrderFilters';
 import './Category.scss';
 import { entityTypesSelector } from '../../../selectors/entityTypesSelector';
 import { loadCategoryAction } from '../../../redux/actions/EntityTypesAction';
 import { ICategory } from '../../../types/api';
+import EntityListContainer from '../../entity-list-container/EntityListContainer';
 
 const selectorData = [
   {
@@ -26,68 +23,46 @@ const selectorData = [
 const Category = () => {
   const pageLimit = 10;
   const dispatch = useDispatch();
-  const [currentPage, setCurrentPage] = useState(1);
   const { category, isLoading } = useSelector(entityTypesSelector);
 
   useEffect(() => {
     dispatch(loadCategoryAction());
   }, []);
 
-  const tableClassName = cn('category__info__table', {
-    category__info__table_loading: isLoading,
-  });
-
-  const handlePaginationChange = (page: number) => {
-    setCurrentPage(page);
-  };
+  const table = (
+    <table>
+      <thead>
+        <tr>
+          <th>Название</th>
+          <th>Описание</th>
+          <th>Последнее обновление</th>
+        </tr>
+      </thead>
+      <tbody>
+        {category.data.map((categoryInfo: ICategory, index: number) => (
+          <tr
+            key={index}
+            onClick={() => {}}
+          >
+            <td>{categoryInfo.name}</td>
+            <td>{categoryInfo.description}</td>
+            <td>{moment(categoryInfo.updatedAt).format('DD MMMM YYYY')}</td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  );
 
   return (
-    <main className="category">
-      <h2>Категории автомобилей</h2>
-      <section className="category__info">
-        <div className="category__info__filters">
-          <OrderFilters selectorData={selectorData} />
-        </div>
-        <div className={tableClassName}>
-          {isLoading
-            ? <Spinner />
-            : (
-              <table>
-                <thead>
-                  <tr>
-                    <th>Название</th>
-                    <th>Описание</th>
-                    <th>Последнее обновление</th>
-                  </tr>
-                </thead>
-                <tbody>
-                  {category.data.map((categoryInfo: ICategory, index: number) => (
-                    <tr
-                      key={index}
-                      onClick={() => {}}
-                    >
-                      <td>{categoryInfo.name}</td>
-                      <td>{categoryInfo.description}</td>
-                      <td>{moment(categoryInfo.updatedAt).format('DD MMMM YYYY')}</td>
-                    </tr>
-                  ))}
-                </tbody>
-              </table>
-            )}
-        </div>
-        <div className="category__pagination">
-          <Pagination
-            current={currentPage}
-            size="small"
-            total={category.count}
-            showSizeChanger={false}
-            pageSize={pageLimit}
-            onChange={handlePaginationChange}
-            disabled={category.count <= pageLimit}
-          />
-        </div>
-      </section>
-    </main>
+    <>
+      <EntityListContainer
+        childComponent={table}
+        dataCount={category.count}
+        filterFields={selectorData}
+        isLoading={isLoading}
+        pageLimit={pageLimit}
+      />
+    </>
   );
 };
 
--- src/components/admin-content/pick-up-point/PickUpPoint.tsx
+++ src/components/admin-content/pick-up-point/PickUpPoint.tsx
@@ -1,14 +1,11 @@
-import { Pagination } from 'antd';
 import React, { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import cn from 'classnames';
 import { getPointsAction } from '../../../redux/actions/PointsDataAction';
 import { pointsDataSelector } from '../../../selectors/pointsDataSelector';
 import { ICityInfo, IPoint } from '../../../types/api';
 import { limitPerPage } from '../../../utils/LimitPerPage';
-import Spinner from '../../Spinner/Spinner';
-import OrderFilters from '../order-filters/OrderFilters';
 import './PickUpPoints.scss';
+import EntityListContainer from '../../entity-list-container/EntityListContainer';
 
 const selectorData = [
   {
@@ -30,9 +27,39 @@ const PickUpPoints = () => {
   const dispatch = useDispatch();
   const pointsDataState = useSelector(pointsDataSelector);
 
-  const tableClassName = cn('points__info__table', {
-    points__info__table_loading: pointsDataState.isLoading,
-  });
+  const table = (
+    <table>
+      <thead>
+        <tr>
+          <th>Город</th>
+          <th>Пункты выдачи</th>
+          <th>Адрес пункта выдачи</th>
+        </tr>
+      </thead>
+      <tbody>
+        {elementsOnPage.map((cityInfo: ICityInfo, index: number) => (
+          <tr
+            key={index}
+            onClick={() => {}}
+          >
+            <td>{cityInfo.name}</td>
+            <td>
+              {pointsDataState.data.map((point: IPoint) => {
+                if (point.cityId && point.cityId.id === cityInfo.id) return <div>{point.name}</div>;
+                return null;
+              })}
+            </td>
+            <td>
+              {pointsDataState.data.map((point: IPoint) => {
+                if (point.cityId && point.cityId.id === cityInfo.id) return <div>{point.address}</div>;
+                return null;
+              })}
+            </td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  );
 
   useEffect(() => {
     dispatch(getPointsAction('admin'));
@@ -46,67 +73,17 @@ const PickUpPoints = () => {
     setElementsOnPage(limitPerPage(pointsDataState.cities.data, currentPage - 1, pageLimit));
   }, [currentPage]);
 
-  const handlePaginationChange = (page: number) => {
-    setCurrentPage(page);
-  };
-
   return (
-    <main className="points">
-      <h2>Список пунктов выдачи</h2>
-      <section className="points__info">
-        <div className="points__info__filters">
-          <OrderFilters selectorData={selectorData} />
-        </div>
-        <div className={tableClassName}>
-          {pointsDataState.isLoading
-            ? <Spinner />
-            : (
-              <table>
-                <thead>
-                  <tr>
-                    <th>Город</th>
-                    <th>Пункты выдачи</th>
-                    <th>Адрес пункта выдачи</th>
-                  </tr>
-                </thead>
-                <tbody>
-                  {elementsOnPage.map((cityInfo: ICityInfo, index: number) => (
-                    <tr
-                      key={index}
-                      onClick={() => {}}
-                    >
-                      <td>{cityInfo.name}</td>
-                      <td>
-                        {pointsDataState.data.map((point: IPoint) => {
-                          if (point.cityId && point.cityId.id === cityInfo.id) return <div>{point.name}</div>;
-                          return null;
-                        })}
-                      </td>
-                      <td>
-                        {pointsDataState.data.map((point: IPoint) => {
-                          if (point.cityId && point.cityId.id === cityInfo.id) return <div>{point.address}</div>;
-                          return null;
-                        })}
-                      </td>
-                    </tr>
-                  ))}
-                </tbody>
-              </table>
-            )}
-        </div>
-        <div className="points__pagination">
-          <Pagination
-            current={currentPage}
-            size="small"
-            total={pointsDataState.cities.count}
-            showSizeChanger={false}
-            pageSize={pageLimit}
-            onChange={handlePaginationChange}
-            disabled={pointsDataState.cities.count <= pageLimit}
-          />
-        </div>
-      </section>
-    </main>
+    <>
+      <EntityListContainer
+        childComponent={table}
+        dataCount={pointsDataState.cities.count}
+        filterFields={selectorData}
+        isLoading={pointsDataState.isLoading}
+        pageLimit={pageLimit}
+        setCustomCurrentPage={setCurrentPage}
+      />
+    </>
   );
 };
 
